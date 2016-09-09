b4af75920ef5645cd8ec391e94d078de21f52fba
Hélio Évora <helio.evora@trytecom.org>
2016-09-09 15:48:13 +0100

    Projecto from scratch

---
+++ src/main/java/com/trytecom/App.java
@@ -0,0 +1,13 @@
+package com.trytecom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
